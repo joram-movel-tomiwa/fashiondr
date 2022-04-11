8f4018b384041baa3ddecb348ea0b75089a61d8b
oolumuyiwa <oolumuyiwa@vassaar.edu>
2022-04-10 22:32:45 -0400

    Initial Commit

---
+++ Fashiondr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fashiondr
+//
+//  Created by Oluwatomiwa Olumuyiwa on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
